c594947856a610dddc839113ab11f29fef7395d4
Munzer221 <altcoinkid@gmail.com>
2025-12-31 02:10:43 -0500

    auto-save 2025-12-31_07-10-41

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -87,9 +87,11 @@ class _LoginScreenState extends State<LoginScreen> {
 
   /// Generate a random nonce string
   String _generateNonce([int length = 32]) {
-    const charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+    const charset =
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
     final random = Random.secure();
-    return List.generate(length, (_) => charset[random.nextInt(charset.length)]).join();
+    return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+        .join();
   }
 
   @override
