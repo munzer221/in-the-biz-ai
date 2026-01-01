8a99e25c339bccb47421dece35ec0f5eb02b7480
Munzer221 <altcoinkid@gmail.com>
2026-01-01 03:37:23 -0500

    auto-save 2026-01-01_08-37-23

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1651,6 +1651,26 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     );
   }
 
+  /// Get the industry-specific section for onboarding template ordering
+  String? _getIndustrySpecificSectionForTemplate(String? industry) {
+    switch (industry) {
+      case 'Rideshare & Delivery':
+        return 'rideshare';
+      case 'Music & Entertainment':
+        return 'music';
+      case 'Artist & Crafts':
+        return 'art';
+      case 'Retail/Sales':
+        return 'retail';
+      case 'Salon/Spa':
+        return 'salon';
+      case 'Hospitality':
+        return 'hospitality';
+      default:
+        return null;
+    }
+  }
+
   Widget _buildTemplateSection({
     required String title,
     required IconData icon,
