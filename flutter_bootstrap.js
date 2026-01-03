bc976e8aafed01d842e7410b8c6903c37c271672
Munzer221 <altcoinkid@gmail.com>
2026-01-03 06:56:54 -0500

    Deploy: WITH kIsWeb fixes from main

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "374844404"
+    serviceWorkerVersion: "822944214"
   }
 });
