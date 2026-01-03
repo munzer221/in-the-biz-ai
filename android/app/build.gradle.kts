17aa7f0d0761dc606cf6d68a7f80f83118de4027
Munzer221 <altcoinkid@gmail.com>
2026-01-03 00:47:27 -0500

    auto-save 2026-01-03_05-47-26

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.in_the_biz_ai"
+    namespace = "com.inthebiz.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -41,7 +41,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.in_the_biz_ai"
+        applicationId = "com.inthebiz.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
